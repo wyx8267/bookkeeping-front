e26a2eaa4ce0f0d24b7ba5094e49c4f887af1533
NEKOI <wyx8267@foxmail.com>
2022-09-12 16:34:10 +0800

    适配移动端

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 export default defineConfig(({ mode }) => {
   return {
     base: mode === 'production' ? '/bookkeeping-front/dist/' : './',
+    server: {
+      origin: 'http://localhost:3000/',
+      host: '0.0.0.0'
+    },
     plugins: [
       vue(),
       vueJsx({
