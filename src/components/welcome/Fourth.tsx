a84aba6e7cfae3d270553cf33a624ad30b38582f
NEKOI <wyx8267@foxmail.com>
2022-09-12 17:40:45 +0800

    displayName

--- src/components/welcome/Fourth.tsx
+++ src/components/welcome/Fourth.tsx
@@ -4,19 +4,19 @@ import cloud from '../../assets/icons/cloud.svg'
 import { WelcomeLayout } from './WelcomeLayout';
 import s from './WelcomeLayout.module.scss'
 
-export const Fourth = {
-  render: () => {
-    const slots = {
-      icon: () => <img src={cloud} />,
-          title: () => <h2>云备份<br />再也不怕数据丢失</h2>,
-          buttons: () => <>
-            <RouterLink to="/start" class={s.fake}>跳过</RouterLink>
-            <RouterLink to="/start">开启应用</RouterLink>
-            <RouterLink to="/start" class={s.fake}>跳过</RouterLink>
-          </>
-    }
-    return (
-      <WelcomeLayout v-slots={slots} />
-    )
+export const Fourth = () => {
+  const slots = {
+    icon: () => <img src={cloud} />,
+    title: () => <h2>云备份<br />再也不怕数据丢失</h2>,
+    buttons: () => <>
+      <RouterLink to="/start" class={s.fake}>跳过</RouterLink>
+      <RouterLink to="/start">开启应用</RouterLink>
+      <RouterLink to="/start" class={s.fake}>跳过</RouterLink>
+    </>
   }
+  return (
+    <WelcomeLayout v-slots={slots} />
+  )
 }
+
+Fourth.displayName = 'Fourth'
