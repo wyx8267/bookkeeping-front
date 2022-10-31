f1f64dac76cb2ec291cca1e0fd972d24cb36e52b
NEKOI <wyx8267@foxmail.com>
2022-10-31 15:30:19 +0800

    滑动切换路由

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,13 +1,27 @@
-import { FunctionalComponent } from 'vue';
+import { defineComponent, ref, watchEffect } from 'vue';
+import { useRouter } from 'vue-router';
+import { useSwipe } from '../../hooks/useSwipe';
 import s from './welcome.module.scss'
 
-export const First: FunctionalComponent = () => {
-  return <div class={s.card}>
-    <svg>
-      <use xlinkHref='#pig'></use>
-    </svg>
-    <h2>会挣钱<br />还要会省钱</h2>
-  </div>
-}
+export const First = defineComponent({
+  setup: () => {
+    const div = ref<HTMLDivElement>()
+    const router = useRouter()
+    const { swiping, direction } = useSwipe(div, {
+      beforeStart: e => e.preventDefault()
+    })
+    watchEffect(() => {
+      if (swiping.value && direction.value === 'left') {
+        router.push('/welcome/2')
+      }
+    })
+    return () => <div class={s.card} ref={div}>
+      <svg>
+        <use xlinkHref='#pig'></use>
+      </svg>
+      <h2>会挣钱<br />还要会省钱</h2>
+    </div>
+  }
+})
 
 First.displayName = 'First'
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -5,12 +5,12 @@ import {useSwipe} from '../hooks/useSwipe'
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    const main = ref<HTMLElement | null>(null)
-    const { direction, swiping } = useSwipe(main)
+    const main = ref<HTMLElement>()
+    // const { direction, swiping } = useSwipe(main)
     
-    watchEffect(() => {
-      console.log(swiping.value, direction.value)
-    })
+    // watchEffect(() => {
+    //   console.log(swiping.value, direction.value)
+    // })
 
     return () => (
       <div class={s.wrapper}>
