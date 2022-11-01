14e43cda73cc1b2023c45733846adec0f8b0e537
NEKOI <wyx8267@foxmail.com>
2022-11-01 12:41:03 +0800

    FloatButton & Icon

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,11 +1,12 @@
 import { defineComponent } from 'vue';
 import { Button } from '../shared/Button';
+import { FloatButton } from '../shared/FloatButton';
 import s from './StartPage.module.scss';
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
     const onClick = () => {
-      console.log('mirai');
+      console.log('test');
       
     }
     return () => (
@@ -13,6 +14,7 @@ export const StartPage = defineComponent({
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>test</Button>
         </div>
+        <FloatButton iconName='add'/>
       </div>
     )
   }
