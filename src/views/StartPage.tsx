16e91b674ef95c5606440146c0511d96ff563df0
NEKOI <wyx8267@foxmail.com>
2022-11-02 18:47:51 +0800

    MainLayout

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,10 +1,10 @@
 import { defineComponent, ref } from 'vue';
 import { RouterLink } from 'vue-router';
+import { MainLayout } from '../layouts/MainLayout';
 import { Button } from '../shared/Button';
 import { Center } from '../shared/Center';
 import { FloatButton } from '../shared/FloatButton';
 import { Icon } from '../shared/Icon';
-import { Navbar } from '../shared/Navbar';
 import { Overlay } from '../shared/Overlay';
 import s from './StartPage.module.scss';
 
@@ -15,23 +15,26 @@ export const StartPage = defineComponent({
       refOverlayVisible.value = !refOverlayVisible.value
     }
     return () => (
-      <div>
-        <Navbar>
-          {{ default: () => '山竹记账', icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu} /> }}
-        </Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name='pig' class={s.pig}></Icon>
-        </Center>
-        <div class={s.button_wrapper}>
-          <RouterLink to='/items/create'>
-            <Button class={s.button} >开始记账</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to='/items/create'>
-          <FloatButton iconName='add' />
-        </RouterLink>
-        {refOverlayVisible.value && <Overlay onClose={onClickMenu} />}
-      </div>
+      <MainLayout>
+        {{
+          title: () => '山竹记账',
+          icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu} />,
+          default: () => <>
+            <Center class={s.pig_wrapper}>
+              <Icon name='pig' class={s.pig}></Icon>
+            </Center>
+            <div class={s.button_wrapper}>
+              <RouterLink to='/items/create'>
+                <Button class={s.button} >开始记账</Button>
+              </RouterLink>
+            </div>
+            <RouterLink to='/items/create'>
+              <FloatButton iconName='add' />
+            </RouterLink>
+            {refOverlayVisible.value && <Overlay onClose={onClickMenu} />}
+          </>
+        }}
+      </MainLayout>
     )
   }
 })
