1fc61eac4b5fe0df3c6dff8111cd891754b059dc
NEKOI <wyx8267@foxmail.com>
2022-11-02 14:02:44 +0800

    Center & Navbar

---
+++ src/shared/Center.tsx
@@ -0,0 +1,25 @@
+import s from './Center.module.scss';
+import { defineComponent, PropType } from 'vue';
+
+const directionMap = {
+  '-' : 'horizontal',
+  '|' : 'vertical',
+  'horizontal' : 'horizontal',
+  'vertical' : 'vertical',
+}
+
+export const Center = defineComponent({
+  props: {
+    direction: {
+      type: String as PropType<'-' | '|' | 'horizontal' | 
+        'vertical'>,
+      default: 'horizontal'
+    }
+  },
+  setup: (props, context) => {
+    const extraClass = directionMap[props.direction]
+    return () => (
+      <div class={[s.center, extraClass]}>{context.slots.default?.()}</div>
+    )
+  }
+})
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,20 +1,29 @@
 import { defineComponent } from 'vue';
 import { Button } from '../shared/Button';
+import { Center } from '../shared/Center';
 import { FloatButton } from '../shared/FloatButton';
+import { Icon } from '../shared/Icon';
+import { Navbar } from '../shared/Navbar';
 import s from './StartPage.module.scss';
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
     const onClick = () => {
       console.log('test');
-      
+
     }
     return () => (
       <div>
+        <Navbar>
+          {{ default: '山竹记账', icon: <Icon name='menu' class={s.navIcon}/> }}
+        </Navbar>
+        <Center class={s.pig_wrapper}>
+          <Icon name='pig' class={s.pig}></Icon>
+        </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>test</Button>
+          <Button class={s.button} onClick={onClick}>开始记账</Button>
         </div>
-        <FloatButton iconName='add'/>
+        <FloatButton iconName='add' />
       </div>
     )
   }
