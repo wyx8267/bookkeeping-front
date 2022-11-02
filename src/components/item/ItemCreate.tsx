9b4a42da3bf494f23d3cd089e52f0f52399a8b71
NEKOI <wyx8267@foxmail.com>
2022-11-02 19:02:33 +0800

    wip: /item/create

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,4 +1,6 @@
 import { defineComponent, PropType } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Icon } from '../../shared/Icon';
 import s from './ItemCreate.module.scss';
 export const ItemCreate = defineComponent({
   props: {
@@ -8,7 +10,12 @@ export const ItemCreate = defineComponent({
   },
   setup: (props, context) => {
     return () => (
-      <div class={s.wrapper}></div>
+      <MainLayout>
+        {{
+          title: () => '记一笔',
+          icon: () => <Icon name='left' class={s.navIcon}/>,
+        }}
+      </MainLayout>
     )
   }
 })
--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,7 +1,7 @@
 import { defineComponent, PropType } from 'vue';
 import s from './Icon.module.scss';
 
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'menu' | 'charts' | 'export' | 'notify'
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'menu' | 'charts' | 'export' | 'notify' | 'left'
 
 export const Icon = defineComponent({
   props: {
