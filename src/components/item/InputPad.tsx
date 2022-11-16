bf84c73e166a25d6acb21ae8883fd18df3ffa6e0
NEKOI <wyx8267@foxmail.com>
2022-11-16 21:08:09 +0800

    time 重构

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,7 +1,7 @@
 import { DatetimePicker, Popup } from 'vant';
 import { defineComponent, PropType, ref } from 'vue';
 import { Icon } from '../../shared/Icon';
-import { time } from '../../shared/time';
+import { Time } from '../../shared/time';
 import s from './InputPad.module.scss';
 
 export const InputPad = defineComponent({
@@ -67,7 +67,7 @@ export const InputPad = defineComponent({
             <Icon name="date" class={s.icon}/>
             <span>
               <span onClick={showDatePicker}>
-                {time(refDate.value).format()}
+                {new Time(refDate.value).format()}
               </span>
               <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
                 <DatetimePicker value={refDate.value} type="date" title="选择年月日" onConfirm={setDate} onCancel={hideDatePicker} />
