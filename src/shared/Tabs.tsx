a4dcdddf878f276837146422d7468a97bf31004b
NEKOI <wyx8267@foxmail.com>
2022-11-16 20:19:51 +0800

    Tabs组件扩展

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -5,6 +5,9 @@ export const Tabs = defineComponent({
     selected: {
       type: String as PropType<string>,
       required: false
+    },
+    classPrefix: {
+      type: String
     }
   },
   setup: (props, context) => {
@@ -16,11 +19,15 @@ export const Tabs = defineComponent({
           throw new Error('<Tabs> only accepts <Tab> as children')
         }
       }
+      const cp = props.classPrefix
 
-      return <div class={s.tabs}>
-        <ol class={s.tabs_nav}>
+      return <div class={[s.tabs, cp + '_tabs']}>
+        <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
           {tabs.map(item => 
-            <li class={item.props?.name === props.selected ? s.selected : ''} onClick={() => context.emit('update:selected', item.props?.name)}>
+            <li class={[
+              item.props?.name === props.selected ? s.selected : '',
+              cp + '_tabs_nav_item'
+            ]} onClick={() => context.emit('update:selected', item.props?.name)}>
               {item.props?.name}
             </li>)}
         </ol>
