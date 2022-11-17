c2927bd702d3ad653c3e735359e0138cc7f305fd
NEKOI <wyx8267@foxmail.com>
2022-11-17 21:18:54 +0800

    echarts引入，图表封装

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,6 +1,10 @@
 import { defineComponent, PropType, ref } from 'vue';
 import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
+import { LineChart } from './LineChart';
+import { PieChart } from './PieChart';
+import { Bars } from './Bars';
+
 export const Charts = defineComponent({
   props: {
     startDate: {
@@ -14,12 +18,16 @@ export const Charts = defineComponent({
   },
   setup: (props, context) => {
     const category = ref('expenses')
+
     return () => (
       <div class={s.wrapper}>
         <FormItem label='类型' type='select' options={[
           { value: 'expenses', text: '支出' },
           { value: 'income', text: '收入' }
-        ]} />
+        ]} v-model={category.value} />
+        <LineChart />
+        <PieChart />
+        <Bars />
       </div>
     )
   }
