119560926443c3492504371b40951c9ef10b9b17
NEKOI <wyx8267@foxmail.com>
2022-11-30 23:32:52 +0800

    创建标签

--- src/shared/validate.tsx
+++ src/shared/validate.tsx
@@ -43,13 +43,13 @@ function isEmpty(value: null | undefined | string | number | FData) {
 }
 
 export function hasError(errors: Record<string, string[]>) {
-  return Object.values(errors).reduce((result, value) => result + value.length, 0) > 0
-  // let result = false
-  // for (const key in errors) {
-  //   if (errors[key].length > 0) {
-  //     result = true
-  //     break
-  //   }
-  // }
-  // return result
+  // return Object.values(errors).reduce((result, value) => result + value.length, 0) > 0
+  let result = false
+  for (const key in errors) {
+    if (errors[key]?.length > 0) {
+      result = true
+      break
+    }
+  }
+  return result
 }
