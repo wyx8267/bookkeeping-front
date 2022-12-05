9ca9e6ac8b5b0b36754b3918e1a9436970792b81
NEKOI <wyx8267@foxmail.com>
2022-12-05 20:28:12 +0800

    完成饼图

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -11,6 +11,8 @@ const DAY = 24 * 3600 * 1000
 
 type Data1Item = { happen_at: string; amount: number }
 type Data1 = Data1Item[]
+type Data2Item = { tag_id: string; tag: Tag; amount: number }
+type Data2 = Data2Item[]
 
 export const Charts = defineComponent({
   props: {
@@ -35,7 +37,7 @@ export const Charts = defineComponent({
       return Array.from({ length: n }).map((_, i) => {
         const time = new Time(props.startDate + 'T00:00:00.000+0800').add(i, 'day').getTimestamp()
         const item = data1.value[0]
-        const amount = (item && new Date(item.happen_at).getTime() === time) ? data1.value.shift()!.amount : 0
+        const amount = item && new Date(item.happen_at).getTime() === time ? data1.value.shift()!.amount : 0
         return [new Date(time).toISOString(), amount]
       })
     })
@@ -45,10 +47,30 @@ export const Charts = defineComponent({
         happen_after: props.startDate,
         happen_before: props.endDate,
         kind: kind.value,
+        group_by: 'happen_at',
         _mock: 'itemSummary'
       })
       data1.value = response.data.groups
     })
+
+    const data2 = ref<Data2>([])
+    const betterData2 = computed<{ name: string; value: number }[]>(() =>
+      data2.value.map((item) => ({
+        name: item.tag.name,
+        value: item.amount
+      }))
+    )
+    onMounted(async () => {
+      const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
+        happen_after: props.startDate,
+        happen_before: props.endDate,
+        kind: kind.value,
+        group_by: 'tag_id',
+        _mock: 'itemSummary'
+      })
+      data2.value = response.data.groups
+    })
+
     return () => (
       <div class={s.wrapper}>
         <FormItem
@@ -61,7 +83,7 @@ export const Charts = defineComponent({
           v-model={kind.value}
         />
         <LineChart data={betterData1.value} />
-        <PieChart />
+        <PieChart data={betterData2.value} />
         <Bars />
       </div>
     )
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -145,15 +145,29 @@ export const mockItemIndexBalance: Mock = (config) => {
 }
 
 export const mockItemSummary: Mock = (config) => {
-  return [
-    200,
-    {
-      groups: [
-        { happen_at: '2022-12-18T00:00:00.000+0800', amount: 100 },
-        { happen_at: '2022-12-22T00:00:00.000+0800', amount: 300 },
-        { happen_at: '2022-12-29T00:00:00.000+0800', amount: 200 }
-      ],
-      summary: 600
-    }
-  ]
+  if (config.params.group_by === 'happen_at') {
+    return [
+      200,
+      {
+        groups: [
+          { happen_at: '2022-12-18T00:00:00.000+0800', amount: 100 },
+          { happen_at: '2022-12-22T00:00:00.000+0800', amount: 300 },
+          { happen_at: '2022-12-29T00:00:00.000+0800', amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  } else {
+    return [
+      200,
+      {
+        groups: [
+          { tag_id: 1, tag: { id: 1, name: '交通' }, amount: 100 },
+          { tag_id: 2, tag: { id: 2, name: '吃饭' }, amount: 300 },
+          { tag_id: 3, tag: { id: 3, name: '购物' }, amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  }
 }
