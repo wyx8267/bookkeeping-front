a8c16420fd3d5b400a90e6c0b2a9f8f516d95d77
NEKOI <wyx8267@foxmail.com>
2022-12-09 23:44:55 +0800

    前端部署设置修改

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { svgstore } from './src/vite_plugins/svgstore';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/bookkeeping-front/dist/' : './',
     server: {
       origin: 'http://localhost:3000',
       host: '0.0.0.0',
